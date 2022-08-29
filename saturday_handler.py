1dfee19ba78c2047862e4e3c174afbb5a94339f7
denblackson <denblackson@ukr.net>
2022-08-29 23:41:57 +0300

    причесав

--- saturday_handler.py
+++ saturday_handler.py
@@ -1,5 +1,6 @@
 import datetime
 
+
 def add_weekday(weekday: tuple):
     weekday_list = [weekday[0], weekday[1]]
     weekday_list[1] += 1
@@ -12,7 +13,7 @@ def add_weekday(weekday: tuple):
         weekday_list[0] = 1
 
     return weekday_list[0], weekday_list[1]
-    
+
 
 def saturday_week_day(saturday: datetime.date):
     first_saturday = datetime.datetime(2022, 8, 27)
@@ -25,4 +26,8 @@ def saturday_week_day(saturday: datetime.date):
         first_saturday_value = add_weekday(first_saturday_value)
 
     return first_saturday_value[0], datetime.date(2022, 8, first_saturday_value[1] + 1).strftime('%A')
-     
+
+
+today_date = datetime.datetime.today().date()
+tomorrow_date = today_date + datetime.timedelta(days=1)
+print(saturday_week_day(tomorrow_date))
