52e13dbe1f132e56ac59e0c2c575d853108dd1ff
denblackson <denblackson@ukr.net>
2022-09-05 11:42:55 +0300

    fix

--- count_week.py
+++ count_week.py
@@ -3,11 +3,13 @@ import datetime
 
 def get_num_of_week():
     datetime_september = datetime.datetime(year=2022, month=8, day=22)
-    num_of_week_in_year = datetime.datetime(year=2022, month=8, day=29).isocalendar().week
+    num_of_week_in_year = datetime.datetime(year=2022, month=8, day=22).isocalendar().week
     week = None
 
     if num_of_week_in_year % 2 == 0:
         week = "2"
     else:
         week = "1"
+    print(week)
     return week
+
--- tetsttt.py
+++ tetsttt.py
@@ -3,7 +3,3 @@ import json
 import requests
 from googletrans import Translator
 
-answer = requests.get('https://yesno.wtf/api')
-print(answer)
-yes_or_not = json.loads(answer.text)
-print(yes_or_not)
