5ec565e2053830e93de206e6d29c156d9c646177
denblackson <denblackson@ukr.net>
2022-09-16 22:29:28 +0300

    update codes

--- count_week.py
+++ count_week.py
@@ -5,6 +5,7 @@ def get_num_of_week(date):
     #datetime_september = datetime.datetime(year=2022, month=8, day=22)
     num_of_week_in_year = date.isocalendar().week
     # num_of_week_in_year = datetime.datetime.now().date().isocalendar().week
+
     week = None
     if num_of_week_in_year % 2 == 0:
         week = "2"
